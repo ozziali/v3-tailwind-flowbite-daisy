070ea6c14372572b9913c012437522932fcec422
ozziali <ozziali@outlook.com>
2023-09-05 02:49:59 +0100

    first commit

---
+++ vue-tail-flow-daisy/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./index.html",
+      "./src/*.{vue, js, jsx, ts, tsx}",
+      "./src/**/*.{vue, js, jsx, ts, tsx}",
+      "./node_modules/flowbite/**/*.js"
+      
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require ('flowbite'),
+    require ('daisyui')
+  ],
+}
+
